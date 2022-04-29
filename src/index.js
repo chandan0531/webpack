4219980d91cb2f509a2bd8542449d8f9331c9e18
chandan0531 <chandan0521@gmail.com>
2022-04-29 23:45:36 +0530

    check

--- src/index.js
+++ src/index.js
@@ -8,13 +8,27 @@ container.innerHTML = navbar();
 
 
 // const url = "https://api.themoviedb.org/3/movie/popular?api_key=5147197db67754942e4353ea8fa56c34&language=en-US&page=1"
-// fetch(url)
-// .then(function(res) {
-//     return res.json()
-// })
-// .then(function(res) {
-//     console.log("res:", res)
-// })
-// .catch(function(err) {
-//     console.log(err)
-// })
+fetch("https://api.themoviedb.org/3/search/movie?api_key=35f6f6d2bcab96e077dfc8302b2ea506&language=en-US&page=1&include_adult=false&query=avenger")
+.then(function (res) {
+    return res.json();
+}).then(function (res) {
+        // return res.results;
+        console.log(res.results);
+        append(res.results);
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
+
+    function append(data){
+        data.map(function(ele){
+            let box=document.createElement("div");
+            let image=document.createElement("img");
+            let imageurl ="https://image.tmdb.org/t/p/w500"
+            image.src = imageurl+ele.poster_path;
+            let p=document.createElement("p");
+            p.innerText=ele.title;
+            box.append(image,p);
+            document.querySelector("#container").append(box);
+        })
+    }
